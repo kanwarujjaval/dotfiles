3fd515ad4bfe3591950cab44d3f00fa01b813481
Kanwar Ujjaval Singh <4216199+kanwarujjaval@users.noreply.github.com>
2026-03-15 22:41:00 +0530

    fixes

--- .chezmoiscripts/run_once_after_install-tpm.sh
+++ .chezmoiscripts/run_once_after_install-tpm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# Install TPM (Tmux Plugin Manager) if not present
+# Install TPM (Tmux Plugin Manager) and plugins
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
   git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+fi
+
+# Install plugins non-interactively
+if [ -x "$HOME/.tmux/plugins/tpm/bin/install_plugins" ]; then
+  "$HOME/.tmux/plugins/tpm/bin/install_plugins"
 fi
